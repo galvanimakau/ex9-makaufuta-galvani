4cd8570743b990981e986e7d31ef6cbbecf4c253
galvani makau futa <r590968@student.thomasmore.be>
2016-12-14 13:29:32 +0100

    de basis url verkorten

--- ex8-makaufuta-galvani/public_html/werking.js
+++ ex8-makaufuta-galvani/public_html/werking.js
@@ -7,3 +7,6 @@
 //data importeren naar module
 var request = require ("request");
 var dal = require('./storage.js');
+
+// verenvoudigen van url
+var BASE_URL = "https://web-ims.thomasmore.be/datadistribution/API/2.0";
