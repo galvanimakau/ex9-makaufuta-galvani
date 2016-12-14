62d33b25be8aaaae9addd9fc0cebe752fd0375d5
galvani makau futa <r590968@student.thomasmore.be>
2016-12-14 13:33:13 +0100

    variabelen drone, file en content indentifieren

--- ex8-makaufuta-galvani/public_html/werking.js
+++ ex8-makaufuta-galvani/public_html/werking.js
@@ -10,3 +10,8 @@ var dal = require('./storage.js');
 
 // verenvoudigen van url
 var BASE_URL = "https://web-ims.thomasmore.be/datadistribution/API/2.0";
+
+//variabelen indentifieren
+var Drone = 0;
+var File = 0;
+var Content = 0;
