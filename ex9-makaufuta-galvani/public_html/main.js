62ba2e49829ac228654026d1a484bc628e1b20ed
galvani makau futa <r590968@student.thomasmore.be>
2017-01-02 12:39:37 +0100

    data importeren naar module

---
+++ ex9-makaufuta-galvani/public_html/main.js
@@ -0,0 +1,4 @@
+//data importeren naar module
+var express = require('express');//WEBSERVER NODE JS
+var parser = require('body-parser');//BODY UIT LEZEN, EXTENSIE OP EXPRESS
+var mongoose = require('mongoose');//GELIJKACHTIG AAN MONGOCLIENT --> JONAS
