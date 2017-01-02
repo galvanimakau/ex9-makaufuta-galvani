3f272717e2824e5321ca773e9ac013ea658c50c1
galvani makau futa <r590968@student.thomasmore.be>
2017-01-02 12:45:49 +0100

    connectie naar mongoose

--- ex9-makaufuta-galvani/public_html/main.js
+++ ex9-makaufuta-galvani/public_html/main.js
@@ -2,3 +2,8 @@
 var express = require('express');//WEBSERVER NODE JS
 var parser = require('body-parser');//BODY UIT LEZEN, EXTENSIE OP EXPRESS
 var mongoose = require('mongoose');//GELIJKACHTIG AAN MONGOCLIENT --> JONAS
+
+//connectie naar mongoose
+mongoose.connect('mongodb://localhost:27017/MyAPI');//DATABANK IN ROBOMONGO
+
+
