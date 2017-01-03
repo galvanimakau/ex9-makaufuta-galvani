56f2362222d75b151404296a33c4c973ea825380
galvani makau futa <r590968@student.thomasmore.be>
2017-01-03 16:09:39 +0100

    schema aanmaken voor aanwezigheden

--- ex9-makaufuta-galvani/public_html/storageAanwezig.js
+++ ex9-makaufuta-galvani/public_html/storageAanwezig.js
@@ -6,3 +6,28 @@
 
 //npm install
 var mongoose = require("mongoose");
+
+//schema aanmaken in mongoose
+var aanwezigSchema = mongoose.Schema({
+    name_drone:{
+        type: String,
+        required: true
+    },
+    name_location:{
+        type: String,
+        required: true
+    },
+    aantal:{
+        type: Number,
+        required: true
+    },
+    uur:{
+        type: String,
+        required: true
+    },
+    ID:{
+        type: Number,
+        required: true,
+        unique: true
+    }
+});
