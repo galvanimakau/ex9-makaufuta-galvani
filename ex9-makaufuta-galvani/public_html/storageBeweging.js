3f0ee0c1233c339cbf627ba9c3f5931a404cef0c
galvani makau futa <r590968@student.thomasmore.be>
2017-01-03 16:23:56 +0100

    schema voor beweging

--- ex9-makaufuta-galvani/public_html/storageBeweging.js
+++ ex9-makaufuta-galvani/public_html/storageBeweging.js
@@ -6,3 +6,31 @@
 
 //npm install
 var mongoose = require("mongoose");
+
+//schema aanmaken in mongoose
+var aanwezigSchema = mongoose.Schema({
+    beweging:{
+        type: Boolean,
+        required: true
+    },
+    begin_location:{
+        name_drone: {type: String},
+        datum_vertrek: {type: Date} 
+    },
+    end_location:{
+        name_drone: {type: String},
+        datum_aankomst: {type: Date}
+    },
+    duur:{
+        type: Number,
+        required: true
+    },
+    beweging_id:{
+        type: Number,
+        required: true,
+        unique: true
+    },
+    weer:{
+        type: String
+    }
+});
